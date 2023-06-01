09e5cd8252fdfc7ff8ce3ee33471ae94d42f9f61
jovylim <jovylimwenjing@gmail.com>
2023-06-01 21:13:09 +0800

    fromt end - set up rough react components, settle theme issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,68 +1,48 @@
 import React, { useEffect, useState } from "react";
+import UserContext from "./context/user";
 import axios from "axios";
+import User from "./context/user";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
 
 function App() {
-  const url = "http://localhost:8000/";
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState([]);
+  const [url, setUrl] = useState("http://localhost:8000/");
+  const [accessToken, setAccessToken] = useState("");
+  const [showLogin, setShowLogin] = useState(true);
+  // const [users, setUsers] = useState([]);
+  // const [user, setUser] = useState([]);
 
-  const getAllUsers = () => {
-    try {
-      axios.get(`${url}routes/get-all-users/`).then((response) => {
-        setUsers(response.data);
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  // const getAllUsers = () => {
+  //   try {
+  //     axios.get(`${url}routes/get-all-users/`).then((response) => {
+  //       setUsers(response.data);
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    getAllUsers();
-  }, []);
+  // useEffect(() => {
+  //   getAllUsers();
+  // }, []);
 
-  useEffect(() => {
-    setUser(users[0]);
-  }, [users]);
+  // useEffect(() => {
+  //   setUser(users[0]);
+  // }, [users]);
 
   return (
-    <div>
-      <div className="navbar bg-primary">
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Link</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100">
-                  <li>
-                    <a>Link 1</a>
-                  </li>
-                  <li>
-                    <a>Link 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <h1>TunedIn</h1>
-      <div>user:{url}</div>
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-        We invest in the world's potential
-      </h1>
-      <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-        Here at Flowbite we focus on markets where technology, innovation, and
-        capital can unlock long-term value and drive economic growth.
-      </p>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <UserContext.Provider value={{ accessToken, setAccessToken, url }}>
+        {accessToken.length > 0 && <Home />}
+        {accessToken.length === 0 && showLogin && (
+          <Login setShowLogin={setShowLogin} />
+        )}
+        {accessToken.length === 0 && !showLogin && (
+          <Registration setShowLogin={setShowLogin} />
+        )}
+      </UserContext.Provider>
+    </>
   );
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,18 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    mytheme: {
-      primary: "#e5965e",
-      secondary: "#3f89e2",
-      accent: "#8188e8",
-      neutral: "#1f2b33",
-      "base-100": "#344451",
-      info: "#92daf6",
-      success: "#4ae88f",
-      warning: "#f7b84a",
-      error: "#fa6b88",
-    },
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#e5965e",
+          secondary: "#3f89e2",
+          accent: "#8188e8",
+          neutral: "#1f2b33",
+          "base-100": "#344451",
+          info: "#92daf6",
+          success: "#4ae88f",
+          warning: "#f7b84a",
+          error: "#fa6b88",
+        },
+      },
+    ],
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
